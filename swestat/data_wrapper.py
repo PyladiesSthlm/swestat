8e796fb8b517ee86db5fc2343695ae54595121c2
luminita <lumi@sbc.su.se>
2013-10-06 10:29:17 +0200

    Pull data from a table is working now

--- swestat/data_wrapper.py
+++ swestat/data_wrapper.py
@@ -33,54 +33,53 @@ def get_url(url):
     except Exception, e:
         sys.exit(str(e))    
 
+def built_post_query(data):
+    """ Given a table metadata built a json query to pull ALL the data """
+    question = {}
+    question['query'] = []
+    for var in data['variables']:
+        val = {}
+        val['code'] = var['code']
+        val['selection'] = {'filter' : 'all', 'values':['*']}
+        question['query'].append(val)
+    question['response'] = {'format': 'json'}
+    return json.dumps(question)
 
-def main():
-    url = "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy"
-    #data = get_url(url)
-    #pprint.pprint(data) 
-    
-    
-    js = json.dumps({   
-    "query": [
-    {          
-     "code": "ContentsCode",
-     "selection": {         
-     "filter": "item",         
-     "values": [           
-     "BE0101N1"         
-    ]       
-   }     
-   },    
-   {       
-    "code": "Tid",
-    "selection": {         
-    "filter": "item",         
-    "values": [           
-    "2010",           
-    "2011"         
-    ]       
-    }     
-   }    
-  ],   
-  "response": {     
-  "format": "csv",   
-  } 
-    })
+"""
+{ "query": [{"code":"Fodelseland", "selection":{"filter":"item", 
+ "values":["010","020"]}}, 
+ {"code":"Alder", "selection":{"filter":"all", "values":["*"]}}, 
+ {"code":"Tid", "selection":{ "filter":"top", "values":["3"]}}], 
+"response": {"format":"csv"} 
+}
+"""    
 
-    #data = urllib.urlencode(js)
-    req = urllib2.Request("http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy", js)
-    response = urllib2.urlopen(req)
-    print response.geturl()
-    print response.info()
-    print type(response)
-    data = response.read()
-    #for d in data:
-    #  print d
-    print data
-    outf = open("XXX", "w")
+def pull_data(url):
+   """ Given an URL including table metadata, get this metadata, then pull 
+   all the data """
+   # get the metadata of the table 
+   table_meta_data = get_url(url)
+   # build a json query to pool all the data
+   json_query = built_post_query(table_meta_data) 
+   # send a POST request 
+   req = urllib2.Request(url, json_query)
+   # get the response 
+   response = urllib2.urlopen(req)
+   data = response.read()
+   return data    
+
+def write_data2file(outfile, data):
+    outf = open(outfile, "w")
     outf.write(data)
     outf.close()
-    
+
+
+def main():
+    url = "http://api.scb.se/OV0104/v1/doris/en/ssd/BO/BO0201/BO0201A/KostnaderPerAreorFH2"
+    data = pull_data(url)
+    print data
+    write_data2file("XXX", data)
+
 
 if __name__ == "__main__":
     main()
