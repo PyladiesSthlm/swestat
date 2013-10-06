df238e5e23f48eca76ec67092b437545deebb37d
luminita <lumi@sbc.su.se>
2013-10-06 14:22:00 +0200

    Very ugly code to plot some of the extracted data

--- swestat/data_wrapper.py
+++ swestat/data_wrapper.py
@@ -9,6 +9,10 @@ import json
 import urllib2
 import pprint
 import urllib
+import ast
+import os
+import matplotlib.pyplot as plt
+
 
 class MyError(Exception): 
     def __init__(self, value):
@@ -66,19 +70,119 @@ def pull_data(url):
    # get the response 
    response = urllib2.urlopen(req)
    data = response.read()
-   return data    
+   return ast.literal_eval(data)    
 
 def write_data2file(outfile, data):
     outf = open(outfile, "w")
     outf.write(data)
     outf.close()
 
+def isFloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def plot_data(labels, content_data, out_file, ylabel):
+    time_data = [(int(labels[i]), float(content_data[i])) \
+                  for i in range(0, len(content_data)) \
+                  if isFloat(content_data[i])]
+    time_data.sort(key = lambda p: p[0])    
+    if len(time_data) < 7:
+        print content_data
+        return 
+    plt.plot(range(0, len(time_data)), [d[1] for d in time_data], \
+                 "b-", linewidth = 2.0)
+    step = len(time_data) / 7
+    plt.xticks(range(0, len(time_data), step), \
+                   [str(time_data[j][0]) for j in range(0, len(time_data), step)])
+    plt.ylabel(ylabel, fontsize = "20")
+    plt.savefig(out_file+".pdf", format = "pdf")
+    plt.close()
+
+
+def plot_col_per_time(data, out_folder):
+    """ Given a data dictionary, find if a period exists. If it does, then make plots 
+    with all the columns of c type """
+    i = 0
+    t = 0 
+    c = 0
+    time_columns = []
+    content_columns = []
+    d_columns = []
+    while i < len(data["columns"]):
+        col = data["columns"][i]
+        if "type" in col:
+            if col["type"] == "t":
+                time_columns.append((t, col["code"], col["text"]))
+                t += 1
+            elif col["type"] == "c":
+                content_columns.append((c, col["code"], col["text"]))
+                c += 1
+            else:
+                d_columns.append((t, col["code"], col["text"]))
+                t += 1
+        else:
+            d_columns.append((t, col["code"], col["text"]))    
+            t += 1
+        i += 1
+    print time_columns
+    print content_columns
+ 
+    # set the value of all the other d and t columns as the first value 
+    set_cols = []
+    for tup in time_columns[1:] + d_columns:
+        v1 = data["data"][0]["key"][tup[0]]
+        set_cols.append((tup[0], tup[1], tup[2], v1))
+
+    filtered_data = []
+    for d in data["data"]:
+        corr = True
+        for sc in set_cols:
+            id = sc[0]
+            v = sc[3]
+            if d["key"][id] != v:
+                corr = False
+                break
+        if corr:
+            filtered_data.append(d)
+
+    print filtered_data
+    data  = filtered_data 
+
+
+    i = time_columns[0][0]
+    labels = [d["key"][i] for d in data]
+    
+    if len(time_columns) > 0 and len(content_columns) > 0:
+        tcol = time_columns[0]
+        i = 0
+        while i < len(content_columns):
+            outfile = os.path.join(out_folder, content_columns[i][1])
+            j = content_columns[i][0]
+            content_data = [d["values"][j] for d in data]
+            plot_data(labels, content_data, outfile, content_columns[i][2])
+            i += 1
+
+        
+    
+
+    
+
 
 def main():
     url = "http://api.scb.se/OV0104/v1/doris/en/ssd/BO/BO0201/BO0201A/KostnaderPerAreorFH2"
     data = pull_data(url)
-    print data
-    write_data2file("XXX", data)
+    print type(data)
+    for col in  data["columns"]:
+        print col["text"], " ", col["type"] 
+    print "-------------"
+    plot_col_per_time(data, "figures")
+
+
+    #print len(data['columns'])
+    #write_data2file("XXX", data)
 
 
 if __name__ == "__main__":
